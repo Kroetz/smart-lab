a23da8fb94456860f9be688d0b9013454f972c47
Dennis Schock <d.schock@muc.qaware.local>
2018-05-16 17:09:24 +0200

    Split room and trigger module into an application module and a library module

---
+++ Code/smart-lab/app-modules/smart-lab-room-microservice/src/test/java/de/qaware/smartlabroommicroservice/SmartLabRoomMicroserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package de.qaware.smartlabroommicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartLabRoomMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ Code/smart-lab/app-modules/smart-lab-trigger-microservice/src/main/java/de/qaware/smartlabtriggermicroservice/SmartLabTriggerMicroserviceApplication.java
@@ -0,0 +1,20 @@
+package de.qaware.smartlabtriggermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "de.qaware.smartlabcommons.api.client")
+@SpringBootApplication(scanBasePackages={
+		"de.qaware.smartlabtrigger",
+		"de.qaware.smartlabcommons",
+		"de.qaware.smartlabcore.data",
+		"de.qaware.smartlabcore.generic"})
+public class SmartLabTriggerMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLabTriggerMicroserviceApplication.class, args);
+	}
+}
