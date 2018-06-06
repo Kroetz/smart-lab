d24cda81afa1856bef330359b18c186241337cc2
Dennis Schock <d.schock@muc.qaware.local>
2018-06-06 10:44:42 +0200

    Repair delegate microservice by scanning for API module components
    * This is a temporary workaround since the delegate module should not need components from the API module because it never calls other sevices

--- Code/smart-lab/app-modules/smart-lab-delegate-microservice/src/main/java/de/qaware/smartlabdelegatemicroservice/SmartLabDelegateMicroserviceApplication.java
+++ Code/smart-lab/app-modules/smart-lab-delegate-microservice/src/main/java/de/qaware/smartlabdelegatemicroservice/SmartLabDelegateMicroserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackageClasses = {
 		de.qaware.smartlabcore.ComponentScanMarker.class,
+		de.qaware.smartlabapi.ComponentScanMarker.class,		// TODO: API should not be needed in the delegate module
 		de.qaware.smartlabdelegate.ComponentScanMarker.class,
 		de.qaware.smartlabaction.ComponentScanMarker.class})
 public class SmartLabDelegateMicroserviceApplication {
