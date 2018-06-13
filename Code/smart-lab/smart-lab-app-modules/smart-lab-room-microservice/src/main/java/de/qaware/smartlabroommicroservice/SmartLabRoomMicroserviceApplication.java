2c3fff709c639d3c21a3b44aa7b5ed35efb8a593
Dennis Schock <d.schock@muc.qaware.local>
2018-06-13 11:09:12 +0200

    Refactor ordering and indentation of annotations on application and configuration classes

--- Code/smart-lab/smart-lab-app-modules/smart-lab-room-microservice/src/main/java/de/qaware/smartlabroommicroservice/SmartLabRoomMicroserviceApplication.java
+++ Code/smart-lab/smart-lab-app-modules/smart-lab-room-microservice/src/main/java/de/qaware/smartlabroommicroservice/SmartLabRoomMicroserviceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableSmartLabRoomService
+@EnableDiscoveryClient
 public class SmartLabRoomMicroserviceApplication {
 
 	public static void main(String[] args) {
--- Code/smart-lab/smart-lab-lib-modules/smart-lab-assistance/src/main/java/de/qaware/smartlabassistance/configuration/AssistanceInfosConfiguration.java
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-assistance/src/main/java/de/qaware/smartlabassistance/configuration/AssistanceInfosConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackageClasses = {
-        de.qaware.smartlabassistance.assistance.info.ComponentScanMarker.class})
+@ComponentScan(basePackageClasses = de.qaware.smartlabassistance.assistance.info.ComponentScanMarker.class)
 @EnableSmartLabCore
 public class AssistanceInfosConfiguration { }
