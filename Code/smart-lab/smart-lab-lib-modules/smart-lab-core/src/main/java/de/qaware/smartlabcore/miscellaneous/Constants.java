750884ad11d4ca8c6f660ad4e96fabf4505fd13b
Dennis Schock <d.schock@muc.qaware.local>
2018-06-24 20:37:39 +0200

    Remove unnecessary constants

--- Code/smart-lab/smart-lab-lib-modules/smart-lab-core/src/main/java/de/qaware/smartlabcore/miscellaneous/Constants.java
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-core/src/main/java/de/qaware/smartlabcore/miscellaneous/Constants.java
@@ -4,10 +4,6 @@ import java.time.Duration;
 
 public abstract class Constants {
 
-    public static final String MINUTE_TAKING = "minuteTaking";
-    public static final String ROOM_UNLOCKING = "roomUnlocking";
-    public static final String AGENDA_SHOWING = "agendaShowing";
-
     public static final String JSON_TYPE_PROPERTY_NAME = "type";
 
     // TODO: Get from configuration object that loads values from properties
