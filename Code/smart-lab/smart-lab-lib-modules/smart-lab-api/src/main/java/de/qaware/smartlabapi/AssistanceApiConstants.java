7a6fb18210b8af45c96e427ec71937abc5fd7a8b
Dennis Schock <d.schock@muc.qaware.local>
2018-07-12 12:51:26 +0200

    Add path mapping constants for getting the base URL of a service to all services

--- Code/smart-lab/smart-lab-lib-modules/smart-lab-api/src/main/java/de/qaware/smartlabapi/AssistanceApiConstants.java
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-api/src/main/java/de/qaware/smartlabapi/AssistanceApiConstants.java
@@ -10,4 +10,5 @@ public class AssistanceApiConstants {
     public static final String MAPPING_BEGIN = "/{" + PARAMETER_NAME_ASSISTANCE_ID + "}" + "/begin";
     public static final String MAPPING_END = "/{" + PARAMETER_NAME_ASSISTANCE_ID + "}" + "/end";
     public static final String MAPPING_UPDATE = "/{" + PARAMETER_NAME_ASSISTANCE_ID + "}" + "/update";
+    public static final String MAPPING_GET_BASE_URL = "/url";
 }
