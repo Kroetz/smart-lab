94bce3c12f8f4a2974ccf18717f1bec6ab0b28c1
Dennis Schock <d.schock@muc.qaware.local>
2018-07-17 00:15:07 +0200

    Add ActionExecutable class for the action "file closing"

--- Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/executable/file/closing/FileClosingExecutable.java
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/executable/file/closing/FileClosingExecutable.java
@@ -1,10 +1,10 @@
-package de.qaware.smartlabaction.action.executable.file.opening;
+package de.qaware.smartlabaction.action.executable.file.closing;
 
 import de.qaware.smartlabaction.action.executable.deviceadapter.fileassociatedprogram.IFileAssociatedProgramAdapter;
 import de.qaware.smartlabaction.action.executable.generic.AbstractActionExecutable;
-import de.qaware.smartlabaction.action.info.file.opening.FileOpeningInfo;
-import de.qaware.smartlabaction.action.result.UuidActionResult;
-import de.qaware.smartlabaction.action.submittable.file.opening.FileOpeningSubmittable;
+import de.qaware.smartlabaction.action.info.file.closing.FileClosingInfo;
+import de.qaware.smartlabaction.action.result.VoidActionResult;
+import de.qaware.smartlabaction.action.submittable.file.closing.FileClosingSubmittable;
 import de.qaware.smartlabapi.service.connector.delegate.IDelegateService;
 import de.qaware.smartlabapi.service.connector.device.IDeviceManagementService;
 import de.qaware.smartlabcore.data.action.generic.IActionArgs;
@@ -15,20 +15,18 @@ import de.qaware.smartlabcore.exception.UnknownDeviceAdapterException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 @Slf4j
-public class FileOpeningExecutable extends AbstractActionExecutable {
+public class FileClosingExecutable extends AbstractActionExecutable {
 
     private final IDeviceManagementService deviceManagementService;
     private final IResolver<String, IFileAssociatedProgramAdapter> programAdapterResolver;
 
-    public FileOpeningExecutable(
-            FileOpeningInfo fileOpeningInfo,
+    public FileClosingExecutable(
+            FileClosingInfo fileClosingInfo,
             IDeviceManagementService deviceManagementService,
             IResolver<String, IFileAssociatedProgramAdapter> fileAssociatedProgramAdapterResolver) {
-        super(fileOpeningInfo);
+        super(fileClosingInfo);
         this.deviceManagementService = deviceManagementService;
         this.programAdapterResolver = fileAssociatedProgramAdapterResolver;
     }
@@ -37,22 +35,22 @@ public class FileOpeningExecutable extends AbstractActionExecutable {
     public IActionResult execute(String programType, IActionArgs genericActionArgs) {
         // Every action can only handle its own specific argument type.
         // TODO: Move this call somewhere else so that this method always gets the right action args type (parameterized?)
-        FileOpeningSubmittable.ActionArgs actionArgs = convertToSpecificActionArgs(
-                FileOpeningSubmittable.ActionArgs.class,
+        FileClosingSubmittable.ActionArgs actionArgs = convertToSpecificActionArgs(
+                FileClosingSubmittable.ActionArgs.class,
                 genericActionArgs);
         IFileAssociatedProgramAdapter programAdapter = this.programAdapterResolver
                 .resolve(programType)
                 .orElseThrow(UnknownDeviceAdapterException::new);
         if(!programAdapter.hasLocalApi()) throw new IllegalStateException();     // TODO: Better exception
-        UUID programInstanceId = programAdapter.openFile(actionArgs.getFileToOpen());
-        return UuidActionResult.of(programInstanceId);
+        programAdapter.close(actionArgs.getProgramInstanceId());
+        return VoidActionResult.instance();
     }
 
     public IActionResult execute(IActionArgs genericActionArgs, IDelegateService delegateService) {
         // Every action can only handle its own specific argument type.
         // TODO: Move this call somewhere else so that this method always gets the right action args type (parameterized?)
-        FileOpeningSubmittable.ActionArgs actionArgs = convertToSpecificActionArgs(
-                FileOpeningSubmittable.ActionArgs.class,
+        FileClosingSubmittable.ActionArgs actionArgs = convertToSpecificActionArgs(
+                FileClosingSubmittable.ActionArgs.class,
                 genericActionArgs);
         IDevice fileAssociatedProgram = this.deviceManagementService.findOne(actionArgs.getProgramId());
         String programType = fileAssociatedProgram.getType();
@@ -64,7 +62,7 @@ public class FileOpeningExecutable extends AbstractActionExecutable {
                 this.actionInfo.getActionId(),
                 programType,
                 actionArgs);
-        UUID programInstanceId = programAdapter.openFile(actionArgs.getFileToOpen());
-        return UuidActionResult.of(programInstanceId);
+        programAdapter.close(actionArgs.getProgramInstanceId());
+        return VoidActionResult.instance();
     }
 }
