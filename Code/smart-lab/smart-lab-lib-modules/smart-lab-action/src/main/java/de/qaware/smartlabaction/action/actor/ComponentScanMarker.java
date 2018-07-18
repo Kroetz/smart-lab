b0bb00fb330fd5c98fbf33167be9387af781a76d
Dennis Schock <d.schock@muc.qaware.local>
2018-07-18 14:54:36 +0200

    Add missing component scanning

---
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/actor/ComponentScanMarker.java
@@ -0,0 +1,3 @@
+package de.qaware.smartlabaction.action.actor;
+
+public interface ComponentScanMarker { }
--- Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/configuration/ActionServiceConfiguration.java
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/configuration/ActionServiceConfiguration.java
@@ -11,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackageClasses = {
         de.qaware.smartlabaction.service.ComponentScanMarker.class,
         de.qaware.smartlabaction.action.info.ComponentScanMarker.class,
-        de.qaware.smartlabaction.action.executable.ComponentScanMarker.class})
+        de.qaware.smartlabaction.action.executable.ComponentScanMarker.class,
+        de.qaware.smartlabaction.action.actor.ComponentScanMarker.class})
 @EnableSmartLabCore
 @EnableSmartLabApi
 @EnableFeignClients(basePackageClasses = de.qaware.smartlabaction.action.externalapi.ComponentScanMarker.class)
