dd9d50ec9781371f3b8db2ad2003f05c7ff56e1e
Dennis Schock <d.schock@muc.qaware.local>
2018-07-20 15:28:11 +0200

    Add ActionSubmittable class for action "beamer activation"

---
+++ Code/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/submittable/beamer/activation/BeamerActivationSubmittable.java
@@ -0,0 +1,37 @@
+package de.qaware.smartlabaction.action.submittable.beamer.activation;
+
+import de.qaware.smartlabaction.action.info.beamer.activation.BeamerActivationInfo;
+import de.qaware.smartlabaction.action.submittable.generic.AbstractActionSubmittable;
+import de.qaware.smartlabapi.service.connector.action.IActionService;
+import de.qaware.smartlabcore.data.action.generic.IActionArgs;
+import de.qaware.smartlabcore.data.action.generic.result.IActionResult;
+import de.qaware.smartlabcore.data.device.entity.DeviceId;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class BeamerActivationSubmittable extends AbstractActionSubmittable<BeamerActivationSubmittable.ActionArgs, Void> {
+
+    public BeamerActivationSubmittable(BeamerActivationInfo beamerActivationInfo) {
+        super(beamerActivationInfo);
+    }
+
+    public Void submitExecution(IActionService actionService, ActionArgs actionArgs) {
+        IActionResult actionResult = actionService.executeAction(this.actionInfo.getActionId(), actionArgs);
+        return actionResult.getVoidValue();
+    }
+
+    @Data
+    @RequiredArgsConstructor(staticName = "of")     // TODO: Eliminate string literal
+    @NoArgsConstructor // TODO: Really necessary for objects being able to serialize/deserialize?
+    public static class ActionArgs implements IActionArgs {
+
+        @NonNull
+        private DeviceId beamerId;
+    }
+}
