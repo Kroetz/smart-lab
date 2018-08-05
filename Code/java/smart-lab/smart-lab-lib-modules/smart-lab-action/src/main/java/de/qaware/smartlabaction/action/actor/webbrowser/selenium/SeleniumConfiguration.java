0c258baa36edf2ffb7e5bcd5eeeac112623a8583
Dennis Schock <d.schock@muc.qaware.local>
2018-08-05 02:16:35 +0200

    Change prefix of Selenium configuration properties

--- Code/java/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/actor/webbrowser/selenium/SeleniumConfiguration.java
+++ Code/java/smart-lab/smart-lab-lib-modules/smart-lab-action/src/main/java/de/qaware/smartlabaction/action/actor/webbrowser/selenium/SeleniumConfiguration.java
@@ -41,7 +41,7 @@ public class SeleniumConfiguration {
     }
 
     // TODO: String literal
-    @ConfigurationProperties(prefix = "selenium", ignoreInvalidFields = true)
+    @ConfigurationProperties(prefix = "smart-lab.actor.selenium", ignoreInvalidFields = true)
     @Validated
     public static class Properties {
 
