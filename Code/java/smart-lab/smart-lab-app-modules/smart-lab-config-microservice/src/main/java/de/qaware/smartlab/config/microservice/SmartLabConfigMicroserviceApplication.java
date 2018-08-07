e6ba4ca1b1f3f9d1571499e5ea48f9ee017a88d8
Dennis Schock <d.schock@muc.qaware.local>
2018-08-07 20:15:20 +0200

    Refactor package structure of the application module "smart-lab-config-microservice"

--- Code/java/smart-lab/smart-lab-app-modules/smart-lab-config-microservice/src/main/java/de/qaware/smartlab/config/microservice/SmartLabConfigMicroserviceApplication.java
+++ Code/java/smart-lab/smart-lab-app-modules/smart-lab-config-microservice/src/main/java/de/qaware/smartlab/config/microservice/SmartLabConfigMicroserviceApplication.java
@@ -1,4 +1,4 @@
-package de.qaware.smartlabconfigmicroservice;
+package de.qaware.smartlab.config.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Code/java/smart-lab/smart-lab-app-modules/smart-lab-config-microservice/src/test/java/de/qaware/smartlab/config/microservice/SmartLabConfigMicroserviceApplicationTests.java
+++ Code/java/smart-lab/smart-lab-app-modules/smart-lab-config-microservice/src/test/java/de/qaware/smartlab/config/microservice/SmartLabConfigMicroserviceApplicationTests.java
@@ -1,4 +1,4 @@
-package de.qaware.smartlabconfigmicroservice;
+package de.qaware.smartlab.config.microservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
