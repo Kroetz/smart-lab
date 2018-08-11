ac119de8ced086016371324d8939d659f4066c51
Dennis Schock <d.schock@muc.qaware.local>
2018-08-11 20:58:07 +0200

    Remove HttpCrud base interface since Feign clients only support 1 level of inheritance

--- Code/java/smart-lab/smart-lab-lib-modules/smart-lab-api/src/main/java/de/qaware/smartlab/api/service/client/generic/IBasicEntityManagementApiClient.java
+++ Code/java/smart-lab/smart-lab-lib-modules/smart-lab-api/src/main/java/de/qaware/smartlab/api/service/client/generic/IBasicEntityManagementApiClient.java
@@ -1,6 +1,16 @@
 package de.qaware.smartlab.api.service.client.generic;
 
 import de.qaware.smartlab.core.data.generic.IDto;
-import de.qaware.smartlab.core.miscellaneous.IHttpCrudMethods;
+import org.springframework.http.ResponseEntity;
 
-public interface IBasicEntityManagementApiClient<DtoT extends IDto> extends IHttpCrudMethods<DtoT> { }
+import java.util.Set;
+
+public interface IBasicEntityManagementApiClient<DtoT extends IDto> {
+
+    Set<DtoT> findAll();
+    ResponseEntity<DtoT> findOne(String entityId);
+    ResponseEntity<Set<DtoT>> findMultiple(String[] entityIds);
+    ResponseEntity<DtoT> create(DtoT entity);
+    ResponseEntity<Set<DtoT>> create(Set<DtoT> entities);
+    ResponseEntity<Void> delete(String entityId);
+}
--- Code/java/smart-lab/smart-lab-lib-modules/smart-lab-core/src/main/java/de/qaware/smartlab/core/service/controller/IBasicEntityManagementController.java
+++ Code/java/smart-lab/smart-lab-lib-modules/smart-lab-core/src/main/java/de/qaware/smartlab/core/service/controller/IBasicEntityManagementController.java
@@ -1,6 +1,16 @@
 package de.qaware.smartlab.core.service.controller;
 
 import de.qaware.smartlab.core.data.generic.IDto;
-import de.qaware.smartlab.core.miscellaneous.IHttpCrudMethods;
+import org.springframework.http.ResponseEntity;
 
-public interface IBasicEntityManagementController<DtoT extends IDto> extends IHttpCrudMethods<DtoT> { }
+import java.util.Set;
+
+public interface IBasicEntityManagementController<DtoT extends IDto> {
+
+    Set<DtoT> findAll();
+    ResponseEntity<DtoT> findOne(String entityId);
+    ResponseEntity<Set<DtoT>> findMultiple(String[] entityIds);
+    ResponseEntity<DtoT> create(DtoT entity);
+    ResponseEntity<Set<DtoT>> create(Set<DtoT> entities);
+    ResponseEntity<Void> delete(String entityId);
+}
