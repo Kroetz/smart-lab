e8f099d45eded82e0a5e44295cd51ee18eecd0dc
Dennis Schock <d.schock@muc.qaware.local>
2018-08-13 16:17:31 +0200

    Fix a bug where the microphone adapter was not provided to the core system

--- Code/java/smart-lab/smart-lab-lib-modules/smart-lab-actuator-adapter/src/main/java/de/qaware/smartlab/actuator/adapter/adapters/microphone/ThinkpadP50InternalMicrophoneAdapter.java
+++ Code/java/smart-lab/smart-lab-lib-modules/smart-lab-actuator-adapter/src/main/java/de/qaware/smartlab/actuator/adapter/adapters/microphone/ThinkpadP50InternalMicrophoneAdapter.java
@@ -2,11 +2,6 @@ package de.qaware.smartlab.actuator.adapter.adapters.microphone;
 
 import de.qaware.smartlab.core.exception.actuator.ActuatorException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import javax.sound.sampled.AudioFileFormat;
@@ -14,16 +9,16 @@ import javax.sound.sampled.AudioFormat;
 import java.nio.file.Path;
 
 import static java.lang.String.format;
+import static java.util.Objects.isNull;
 
 @Component
-@Conditional(ThinkpadP50InternalMicrophoneAdapter.ConditionalOnAvailable.class)
 @Slf4j
 public class ThinkpadP50InternalMicrophoneAdapter extends AbstractMicrophoneAdapter {
 
     public static final String ACTUATOR_TYPE = "thinkpad p50 internal microphone";
     private static final boolean HAS_LOCAL_API = true;
     // TODO: Do not use microphone name for identification but rather something like the manufacturer that is not affected by localization
-    private static final String MICROPHONE_NAME = "Mikrofonarray (Realtek High Def";
+    private static final String MICROPHONE_NAME = "Mikrofonarray (Realtek High Definition Audio)";
     private static final AudioFormat AUDIO_FORMAT = new AudioFormat(
             AudioFormat.Encoding.PCM_SIGNED,
             44100.0f,
@@ -34,21 +29,16 @@ public class ThinkpadP50InternalMicrophoneAdapter extends AbstractMicrophoneAdap
             false);
     private static final AudioFileFormat.Type AUDIO_FILE_FORMAT = AudioFileFormat.Type.WAVE;
 
-    private final GenericMicrophone genericMicrophone;
+    private GenericMicrophone genericMicrophone;
 
     public ThinkpadP50InternalMicrophoneAdapter() {
         super(ACTUATOR_TYPE, HAS_LOCAL_API);
-        this.genericMicrophone = GenericMicrophone.getMicrophone(MICROPHONE_NAME, AUDIO_FORMAT)
-                .orElseThrow(() -> {
-                    String errorMessage = String.format("Could not get microphone with the name \"%s\"", MICROPHONE_NAME);
-                    log.error(errorMessage);
-                    return new ActuatorException(errorMessage);
-                });
     }
 
     @Override
     public void startRecording(Path recordingTargetFile) throws ActuatorException {
         try {
+            if(isNull(this.genericMicrophone)) this.genericMicrophone = tryGetMicrophone();
             this.genericMicrophone.startRecording(recordingTargetFile, AUDIO_FILE_FORMAT);
         }
         catch(Exception e) {
@@ -61,6 +51,7 @@ public class ThinkpadP50InternalMicrophoneAdapter extends AbstractMicrophoneAdap
     @Override
     public Path stopRecording() throws ActuatorException {
         try {
+            if(isNull(this.genericMicrophone)) this.genericMicrophone = tryGetMicrophone();
             return this.genericMicrophone.stopRecording()
                     .orElseThrow(() -> {
                         String errorMessage = "Could not get recorded audio";
@@ -75,11 +66,12 @@ public class ThinkpadP50InternalMicrophoneAdapter extends AbstractMicrophoneAdap
         }
     }
 
-    public static class ConditionalOnAvailable implements Condition {
-
-        @Override
-        public boolean matches(@NonNull ConditionContext context, @NonNull AnnotatedTypeMetadata metadata) {
-            return GenericMicrophone.isMicrophoneAvailable(ACTUATOR_TYPE, AUDIO_FORMAT);
-        }
+    private GenericMicrophone tryGetMicrophone() {
+        return GenericMicrophone.getMicrophone(MICROPHONE_NAME, AUDIO_FORMAT)
+                .orElseThrow(() -> {
+                    String errorMessage = String.format("Could not get microphone with the name \"%s\"", MICROPHONE_NAME);
+                    log.error(errorMessage);
+                    return new ActuatorException(errorMessage);
+                });
     }
 }
