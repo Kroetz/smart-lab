5d402596833a03094c89f7819a9edf6cc12f6227
Dennis Schock <d.schock@muc.qaware.local>
2018-08-17 16:04:42 +0200

    Move SpringFox configuration into the core module and remove unnecessary configurations

---
+++ code/java/smart-lab/smart-lab-lib-modules/smart-lab-core/src/main/java/de/qaware/smartlab/core/configuration/SpringFoxConfiguration.java
@@ -0,0 +1,66 @@
+package de.qaware.smartlab.core.configuration;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.WildcardType;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+import static springfox.documentation.schema.AlternateTypeRules.newRule;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfiguration {
+
+    private final TypeResolver typeResolver;
+
+    public SpringFoxConfiguration(TypeResolver typeResolver) {
+        this.typeResolver = typeResolver;
+    }
+
+    @Bean
+    public Docket smartLabApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(newRule(
+                        typeResolver.resolve(DeferredResult.class,
+                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                        typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+                .enableUrlTemplating(true);
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .validatorUrl(null)
+                .build();
+    }
+}
